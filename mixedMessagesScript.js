02085b09bffa07f11d4d5999d860956d89106292
OwenDavies97 <ohdavies97@gmail.com>
2021-05-12 16:02:08 +0100

    Added random number function and empty array

--- mixedMessagesScript.js
+++ mixedMessagesScript.js
@@ -4,3 +4,11 @@ const collectiveMessage = {
     horoscopeOutput: ['The best day', 'The worst day', 'A mediocre day', 'Some good news', 'Some bad news'],
     advice: ['Call a friend', 'Go grab a McDonalds', 'Go for a run', 'Sleep all day', 'Watch some netflix']
 };
+
+//Random number function
+const generateRandomNum = (num) => {
+    return Math.floor(Math.random()*num)
+};
+
+//Empty array for horoscope
+let horoscope = [];
