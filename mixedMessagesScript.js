dfd07045ef3313883b105b4d346255ec494b1513
OwenDavies97 <ohdavies97@gmail.com>
2021-05-12 15:50:44 +0100

    Message components added in an object

--- mixedMessagesScript.js
+++ mixedMessagesScript.js
@@ -1,1 +1,6 @@
-//Saving file to repository
+// Object storing message components
+const collectiveMessage = {
+    starSign: ['Nebula', 'Lunar', 'Solar', 'Solstice', 'Equinox'],
+    horoscopeOutput: ['The best day', 'The worst day', 'A mediocre day', 'Some good news', 'Some bad news'],
+    advice: ['Call a friend', 'Go grab a McDonalds', 'Go for a run', 'Sleep all day', 'Watch some netflix']
+};
